dca20abe3fcf7f060dfbc0e9bd697bdf1539a936
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-26 09:38:25 +0100

    Creates models

--- emily/emily/settings.py
+++ emily/emily/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'emilys.apps.EmilysConfig'
 ]
 
 MIDDLEWARE = [
---
+++ emily/emilys/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EmilysConfig(AppConfig):
+    name = 'emilys'
